0fff4235f805ff6da2622f863c18d52018b81999
mutergfp <lucas.desnoue@free.fr>
2021-03-01 14:01:52 +0100

    Ajout crédit scene

--- src/scenes/CreditScene.ts
+++ src/scenes/CreditScene.ts
@@ -1,10 +1,10 @@
 const config: Phaser.Types.Scenes.SettingsConfig = {
-    key: 'menuScene', // The key to provide to Phaser whene you want to change scene to this one
+    key: 'creditScene', // The key to provide to Phaser whene you want to change scene to this one
     visible: false,
     active: false // Defines if the Scene is active or not.
 }
 
-export default class MenuScene extends Phaser.Scene {
+export default class CreditScene extends Phaser.Scene {
 
     constructor() {
         super(config)
@@ -12,7 +12,6 @@ export default class MenuScene extends Phaser.Scene {
 
     public preload(): void {
         this.load.spritesheet('play_button', 'assets/images/ui/play_button.png', { frameWidth: 310, frameHeight: 116, endFrame: 2 });
-        this.load.spritesheet('credits_button', 'assets/images/ui/credits_button.png', { frameWidth: 310, frameHeight: 116, endFrame: 2 });
         this.load.image('backgrondMainMenu', 'assets/images/backgrondMainMenu.png');
         this.load.audio('themeMenu', [
             'assets/sounds/menu.wav'
@@ -27,43 +26,25 @@ export default class MenuScene extends Phaser.Scene {
 
 
     public create(): void {
-        this.game.registry.set('idLevel', 3);
         //this.add.image(this.game.config.width as integer/2, this.game.config.height as integer/2,'backgrondMainMenu')
         const music = this.sound.add('themeMenu', { loop: true, volume: 0.2 });
         music.play();
-        const playButton = this.add.sprite(this.game.config.width as integer / 2, this.game.config.height as integer / 2, 'play_button').setInteractive()
-        const creditsButton = this.add.sprite(this.game.config.width as integer / 2, this.game.config.height as integer / 2 + 150, 'credits_button').setInteractive()
-        playButton.on('pointerover', () => {
-            playButton.setFrame(1);
+        const backButton = this.add.sprite(this.game.config.width as integer / 2, this.game.config.height as integer / 2, 'play_button').setInteractive()
+
+        backButton.on('pointerover', () => {
+            backButton.setFrame(1);
         });
-        playButton.on('pointerout', () => {
-            playButton.setFrame(0);
+        backButton.on('pointerout', () => {
+            backButton.setFrame(0);
         });
-        playButton.once('pointerdown', () => {
-            playButton.setFrame(2)
-            playButton.once('pointerup', () => {
+        backButton.once('pointerdown', () => {
+            backButton.setFrame(2)
+            backButton.once('pointerup', () => {
                 music.stop()
-                this.registry.set('themeMusic', this.sound.add('themeGame', { loop: true, volume: 0.1 }));
-                (this.registry.get('themeMusic') as Phaser.Sound.BaseSound).play()
-                this.scene.start('level-1')
+                this.scene.start('menuScene')
             })
         })
 
-        creditsButton.on('pointerover', () => {
-            creditsButton.setFrame(1);
-        });
-        creditsButton.on('pointerout', () => {
-            creditsButton.setFrame(0);
-        });
-        creditsButton.once('pointerdown', () => {
-            creditsButton.setFrame(2)
-            creditsButton.once('pointerup', () => {
-                music.stop()
-                this.scene.start('creditScene')
-            })
-        })
-
-        music.play();
     }
 
     /**
